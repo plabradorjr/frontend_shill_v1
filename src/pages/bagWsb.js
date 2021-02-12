32f751835093afe3dfea8b6df39fb3b79a8d3fe2
Pancho Labrador <plabradorjr@gmail.com>
2021-02-12 03:07:38 -0600

    yolo added

--- src/pages/bagWsb.js
+++ src/pages/bagWsb.js
@@ -6,7 +6,7 @@ import TradingView from '../components/TradingView';
 import WallsSteetBaby from '../images/wsba.png';
 import PostComment from '../components/PostComment';
 import ShowComments from '../components/ShowComments';
-import loss from '../images/sad-loss.GIF'
+// import loss from '../images/sad-loss.GIF'
 
 
 
@@ -19,25 +19,15 @@ const BagWallStreetBets = () => {
         <NavBar />
         <div className="container">
             <img src={WallsSteetBaby} alt="baby" className="mx-auto d-block img-thumbnail/" style={{height: 80}}></img>
-            <img src={loss} alt="baby" className="mx-auto d-block img-thumbnail/"></img>
+            {/* <img src={loss} alt="baby" className="mx-auto d-block img-thumbnail/"></img> */}
             <div className="row justify-content-center">
+                <ChartStyled >
+                    <h2>YOLO</h2>
+                    <TradingView ticker={"AMEX:YOLO"} />
+                </ ChartStyled>
                 <ChartStyled >
                     <h2>GME</h2>
                     <TradingView ticker={"NYSE:GME"} />
-                </ ChartStyled>
-                <ChartStyled >
-                    <h2>BB</h2>
-                    <TradingView ticker={"NYSE:BB"} />
-                </ ChartStyled>
-            </div>
-            <div className="row">
-                <ChartStyled >
-                    <h2>AMC</h2>
-                    <TradingView ticker={"NYSE:AMC"} />
-                </ ChartStyled>
-                <ChartStyled >
-                    <h2>NOK</h2>
-                    <TradingView ticker={"NYSE:NOK"} />
                 </ ChartStyled>
             </div>
 
