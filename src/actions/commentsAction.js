e54067abc4d53ab7e337e2980e30f6731dee4ca9
Pancho Labrador <plabradorjr@gmail.com>
2022-09-23 02:42:30 -0500

    nav edit

--- src/actions/commentsAction.js
+++ src/actions/commentsAction.js
@@ -1,17 +1,18 @@
-// import axios from 'axios';
+import axios from "axios";
 // note to self, if not using axios, you need to delete ".data" on the payload dispatch
 
 export const fetchComments = () => async (dispatch) => {
+  const allComments = await axios.get("http://localhost:3030/comments");
+  // production site: https://api.shill.lol
 
-    // const allComments = await axios.get("http://localhost:3030/comments")
-    // production site: https://api.shill.lol
+  //   const allComments = await fetch("https://api.shill.lol/comments").then(
+  //     (res) => res.json()
+  //   );
 
-    const allComments = await fetch("https://api.shill.lol/comments").then(res => res.json())
-
-    dispatch({
-        type: "FETCH_COMMENTS",
-        payload: {
-            comments: allComments
-        }
-    })
-}
+  dispatch({
+    type: "FETCH_COMMENTS",
+    payload: {
+      comments: allComments,
+    },
+  });
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,64 +1,91 @@
-import React, {useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCurrentUser, logoutUser } from '../actions/userActions';
-import styled from 'styled-components';
-import { deleteSearchResults } from '../actions/cryptosAction';
-import { Link } from 'react-router-dom';
-
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCurrentUser, logoutUser } from "../actions/userActions";
+import styled from "styled-components";
+import { deleteSearchResults } from "../actions/cryptosAction";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    
-    const user = useSelector((state) => state.userReducer.currentUser)
+  const user = useSelector((state) => state.userReducer.currentUser);
 
-    useEffect(() => {
-       if (!user.is_logged) dispatch(fetchCurrentUser());
-    },[dispatch, user.is_logged]);
+  useEffect(() => {
+    if (!user.is_logged) dispatch(fetchCurrentUser());
+  }, [dispatch, user.is_logged]);
 
+  const handleLogoutClick = (e) => {
+    dispatch(logoutUser());
+    e.preventDefault();
+  };
 
-    const handleLogoutClick = (e) => {
-    
-        dispatch(logoutUser());
-        e.preventDefault();
-    }
+  const handleLoginClick = (e) => {
+    window.location = "https://api.shill.lol/auth/twitter";
+  };
 
-    const handleLoginClick = (e) => {
-        window.location = 'https://api.shill.lol/auth/twitter'
-    }
+  const handleDeleteSearch = () => {
+    dispatch(deleteSearchResults());
+  };
 
-    const handleDeleteSearch = () => {
-        dispatch(deleteSearchResults());
-      }
+  return (
+    <StyledNav>
+      <nav className="navbar navbar-light bgx justify-content-between pt-4 px-5">
+        <div>
+          <Link to="/">
+            {" "}
+            <h4 id="logo" onClick={handleDeleteSearch}>
+              Shill.lol
+            </h4>{" "}
+          </Link>
+          <Link to="/">
+            {" "}
+            <p id="logo" onClick={handleDeleteSearch}>
+              we not gonna make it
+            </p>{" "}
+          </Link>
+        </div>
 
-    return (
-        <StyledNav>
-        <nav className="navbar navbar-light bgx justify-content-between pt-4 px-5">
-            <div>
-                <Link to="/"> <h4 id="logo" onClick={handleDeleteSearch}>Shill.lol</h4> </Link>
-                <Link to="/"> <p id="logo" onClick={handleDeleteSearch}>respec da pamp</p> </Link> 
-            </div>
-           
-            {user.is_logged && <p>Hello,  {user.name}</p>}
+        {user.is_logged && <p>Hello, {user.name}</p>}
 
-            <form className="form-inline">
-                <Link to="/shills"><button className="btn btn-outline-light my-2 mx-2 my-sm-0" type="button">Shills</button></Link>
-                {user.is_logged && <button className="btn btn-outline-light my-2 my-sm-0" type="button" onClick={handleLogoutClick}>Logout</button>}
-                {!user.is_logged && <button className="btn btn-outline-light my-2 my-sm-0" type="button" onClick={handleLoginClick}>Login via Twitter</button>}
-                
-            </form>
-        </nav>
-        </ StyledNav>
-    )
-}
+        <form className="form-inline">
+          <Link to="/shills">
+            <button
+              className="btn btn-outline-light my-2 mx-2 my-sm-0"
+              type="button"
+            >
+              Shills
+            </button>
+          </Link>
+          {user.is_logged && (
+            <button
+              className="btn btn-outline-light my-2 my-sm-0"
+              type="button"
+              onClick={handleLogoutClick}
+            >
+              Logout
+            </button>
+          )}
+          {!user.is_logged && (
+            <button
+              className="btn btn-outline-light my-2 my-sm-0"
+              type="button"
+              onClick={handleLoginClick}
+            >
+              Login via Twitter
+            </button>
+          )}
+        </form>
+      </nav>
+    </StyledNav>
+  );
+};
 
 const StyledNav = styled.div`
-    #logo {
-        cursor: pointer;
-    }
-    .bgx {
-        background-color: #192734;
-    }
-`
+  #logo {
+    cursor: pointer;
+  }
+  .bgx {
+    background-color: #192734;
+  }
+`;
 export default NavBar;
